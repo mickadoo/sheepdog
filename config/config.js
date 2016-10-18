62bd4c2bb52d6ede7f2736a80d8607676e1ed95b
Michael Devery <michaeldevery@gmail.com>
2016-10-18 09:32:10 +0100

    change default rabbitmq host

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    nodeHost: process.env.SHEEPDOG__NODE_HOST || 'localhost',
     nodePort: process.env.SHEEPDOG__NODE_PORT || '3000',
     clientId: process.env.SHEEPDOG__AUTH_CLIENT || 'local',
     clientSecret: process.env.SHEEPDOG__AUTH_SECRET || 'test',
@@ -7,7 +8,7 @@ module.exports = {
     rabbitMq: {
         username: process.env.SHEEPDOG__RABBIT_USER || 'guest',
         password: process.env.SHEEPDOG__RABBIT_PASS || 'guest',
-        host: process.env.SHEEPDOG__RABBIT_HOST || 'rabbit',
+        host: process.env.SHEEPDOG__RABBIT_HOST || 'localhost',
         port: process.env.SHEEPDOG__RABBIT_PORT || '5672',
         queue: process.env.SHEEPDOG__RABBIT_QUEUE || 'yarnyard'
     }
